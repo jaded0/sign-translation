b02f9dcecfdf12c2ee6eb476d2be2315d138af3c
jaden <jexclamation13@gmail.com>
2023-03-12 00:12:59 -0700

    update naming of output_results.txt

--- thru.sh
+++ thru.sh
@@ -3,4 +3,4 @@ module load charliecloud/0.26
 ch-image build --force -t $1 .
 ch-builder2tar $1 ${HOME}
 # mkdir ${HOME}/samples
-sbatch --output ./output_results3.txt --mail-user jaden.lorenc@gmail.com run_vid-signs_tag.sh $1
+sbatch --output ./output_results_$1.txt --mail-user jaden.lorenc@gmail.com run_vid-signs_tag.sh $1
